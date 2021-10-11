6ae20e57140c9764c6e8e908647cdcfa065a7c74
Pablo Padilla <Padilla.Pablo1998@Gmail.com>
2021-10-11 12:51:35 -0700

    added useeffect in componenets

--- src/components/Space.jsx
+++ src/components/Space.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useEffect, useRef, useState } from 'react';
 import '../App.css';
-import { useLoader, useFrame, Canvas } from 'react-three-fiber';
+import { useLoader, useFrame, Canvas, useThree } from 'react-three-fiber';
 import { OrbitControls, Stars, Torus } from "drei";
 import { log, TextureLoader } from 'three';
 import moonPic from '../assets/moon.jpg';
@@ -9,31 +9,40 @@ import pabloPic from '../assets/me-boxed.jpg'
 
 const Space = () => {
 
-    const [goingUp, setGoingUp] = useState(false);
+    const get = useThree();
 
 
-    const PabloCube = () => {
 
-        // const ref = useRef();
+    function PabloCube() {
+
+
+
+        const ref = useRef();
 
         // useFrame((state, delta) => (ref.current.rotation.x += 0.1))
 
         const pabloTexture = useLoader(TextureLoader, pabloPic)
-        let pabloMesh = (
+
+        const rotate = e => {
+            ref.current.rotation.x += 0.01;
+            ref.current.rotation.y += 0.01;
+        }
+
+        document.body.onscroll = rotate;
+
+        return (
             <mesh
                 position={[2, 0, -5]}
-            // ref={ref}
+                ref={ref}
             >
                 <boxGeometry args={[3, 3, 3]} />
                 <meshStandardMaterial
                     map={pabloTexture}
                 />
-            </mesh>)
-
-        return (
-            pabloMesh
+                </mesh>
         )
     }
+
 
     const Moon = () => {
         const moon = useLoader(TextureLoader, moonPic)
@@ -56,17 +65,24 @@ const Space = () => {
     const Box = (props) => {
         // This reference will give us direct access to the mesh
         const boxMesh = useRef();
-        // boxMesh.current.rotation.x += 0.1;
+
         // Set up state for the hovered and active state
         const [hovered, setHover] = useState(false)
         const [active, setActive] = useState(false)
 
-        useFrame(() => {
-
+        const rotate = e => {
             boxMesh.current.rotation.x += 0.01;
             boxMesh.current.rotation.y += 0.01;
+        }
 
-        });
+        document.body.onscroll = rotate;
+
+        // useFrame(() => {
+
+        //     boxMesh.current.rotation.x += 0.01;
+        //     boxMesh.current.rotation.y += 0.01;
+
+        // });
 
         // Subscribe this component to the render-loop, rotate the mesh every frame
         // Return view, these are regular three.js elements expressed in JSX
@@ -84,7 +100,8 @@ const Space = () => {
         )
     }
 
-        
+
+
     function moveCamera() {
         const t = document.body.getBoundingClientRect().top;
         // moon.rotation.x += 0.05;
@@ -97,12 +114,15 @@ const Space = () => {
         // camera.position.z = t * -0.01;
         // camera.position.x = t * -0.0002;
         // camera.rotation.y = t * -0.0002;
-        console.log(Moon);
         console.log(t);
     }
 
     document.body.onscroll = moveCamera;
     moveCamera();
+
+    useEffect(() => {
+        console.log(get);
+    },[get])
 
     return (
 
